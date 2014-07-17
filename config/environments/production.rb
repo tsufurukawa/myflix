5005de0cbeec742486c1df6b735a7206a65d56d7
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-07-17 12:58:32 -0700

    Slight modifications to production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,10 +23,10 @@ Myflix::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'tl-myflix-furukawa.heroku.com',
+    :domain         => 'tl-myflix-furukawa.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'tl-myflix-furukawa' }
+  config.action_mailer.default_url_options = { host: 'tl-myflix-furukawa.herokuapp.com' }
 end
