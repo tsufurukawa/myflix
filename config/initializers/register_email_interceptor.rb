20b1df9e9ae2dfca17a7ac0ddcfd4d3416d0b932
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-08-16 09:55:25 -0700

    Comment out staging email interceptor so email will be sent to real users

--- config/initializers/register_email_interceptor.rb
+++ config/initializers/register_email_interceptor.rb
@@ -1,2 +1,3 @@
 # register interceptor for staging environment
-ActionMailer::Base.register_interceptor(StagingEmailInterceptor) if Rails.env.staging?
+
+# ActionMailer::Base.register_interceptor(StagingEmailInterceptor) if Rails.env.staging?
